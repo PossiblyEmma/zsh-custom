b4c820331424763fe7dd0b529e6db4385a7bd29a
Emma Gray <emma.possibly@gmail.com>
2024-05-24 16:00:56 +0100

    add gri and greset/grh functions

---
+++ git.zsh
@@ -0,0 +1,42 @@
+gri() {
+    amount=${1}
+    if [[ -z $amount ]]
+    then
+        echo "Must provide a number of commits to rebase."
+        echo  "-h for info on this command"
+        return
+    elif [[ $amount = "-h" ]]
+    then
+        echo "Alias for 'git rebase -i HEAD~{amount}'"
+        echo "Must provide a number of commits to rebase"
+        return
+    elif ! [[ $amount =~ "^[0-9]+$" ]]
+    then
+        echo "That is not a number. Must provide a number of commits to rebase."
+        echo "-h for info on this command."
+        return
+    fi
+    git rebase -i HEAD~$amount
+}
+
+greset() {
+    amount=${1}
+    if [[ -z $amount ]]
+    then
+        echo "Must provide a number of commits to reset."
+        echo  "-h for info on this command"
+        return
+    elif [[ $amount = "-h" ]]
+    then
+        echo "Alias for 'git reset HEAD~{amount}'"
+        echo "Must provide a number of commits to reset"
+        return
+    elif ! [[ $amount =~ "^[0-9]+$" ]]
+    then
+        echo "That is not a number. Must provide a number of commits to reset."
+        echo "-h for info on this command."
+        return
+    fi
+    git reset HEAD~$amount
+}
+alias grh="greset"
